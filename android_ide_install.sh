0e67826b84f9c623cf38586a5c1e266c9c01402f
Your Name <you@example.com>
2014-11-08 18:01:28 +0900

    update

--- android_ide_install.sh
+++ android_ide_install.sh
@@ -1,4 +1,6 @@
-sudo apt-get install eclipse -y
+wget http://dl.google.com/android/adt/adt-bundle-linux-x86_64-20140702.zip
+wget http://dl.google.com/android/adt/adt-bundle-linux-x86-20140702.zip
+
 sudo apt-add-repository ppa:paolorotolo/android-studio
 sudo apt-get update
 sudo apt-get install android-studio -y
